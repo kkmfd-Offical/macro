4f01415360133df0a66ba80666ee70f3a08b8f98
luacoder279 <100986647+luacoder279@users.noreply.github.com>
2022-05-11 17:32:39 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+music.play_melody("C5 B C D E F G A ", 120)
+basic.show_icon(IconNames.HEART)
+basic.show_string("welcome")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,3 @@
+music.playMelody("C5 B C D E F G A ", 120)
+basic.showIcon(IconNames.Heart)
+basic.showString("welcome")
